01c3d5eb10f7d8a76e13425c5a8d54bda6da31dc
cdn01 <cdn_01@126.com>
2014-05-12 20:53:09 +0800

    512

---
+++ logs/exception/log-2014-05-12.php
@@ -0,0 +1,66 @@
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:08 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:35 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：Undefined variable: haystack
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第53行：in_array() expects parameter 2 to be array, null given
+2014-05-12 03:34:36 - 错误发生于文件[D:\wamp\www\myblog\core\ck_blog.php]第55行：array_push() expects parameter 1 to be array, null given
